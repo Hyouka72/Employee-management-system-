9f99f7ec3271c9031ce8fa21339e9aea9a77041e
Hyouka45 <khewang212@gmail.com>
2025-07-11 22:08:50 +0545

    yetikai

--- src/main/java/backend/ems/controller/EmployeeController.java
+++ src/main/java/backend/ems/controller/EmployeeController.java
@@ -5,10 +5,9 @@ import backend.ems.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -22,5 +21,10 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedemployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedemployee, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        return ;
     }
 }
